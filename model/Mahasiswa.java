3ddd01867756ff16054f29f8ef5e3cefd05124b8
ruddyprima <primahendra70@gmail.com>
2020-03-29 14:04:41 +0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilaninfo(){
+   System.out.println();
+   System.out.println("==Data Mahasiswa ===============");
+   System.out.println("NPM : 18630864");
+   System.out.println("Nama : Ruddy");
+   System.out.println("==========================");
+ }
+}   
---
+++ model/Matakuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Matakuliah{
+ public static void tampilaninfo(){
+   System.out.println();
+   System.out.println("==Data Mata kuliah ===============");
+   System.out.println("Kode : TI0776786");
+   System.out.println("Nama : Pemprograman berbasis objek 1");
+   System.out.println("=====================================");
+ }
+}   
